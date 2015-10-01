8e9071456a45f96966667aa7bd2a649593acae35
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-01 13:40:25 -0400

    structure now set up properly

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// IIFE = Immediately Invoked Function Expression
+// run the code immediately to make sure code is properly scoped
+// (function () {
+
+    console.log("Hello!");
+
+
+
+
+
+
+
+
+
+// })();
